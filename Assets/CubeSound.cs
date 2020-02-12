0ffb84eac602e51bdc838d4f261d670e9ca702a4
TK140 <yukimitsu.shimakawa@qdenbs.com>
2020-02-12 13:59:24 +0900

    Lesson7の課題結果です。

---
+++ Assets/CubeSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSound : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        //衝突したオブジェクトがグランドの場合
+        if (other.gameObject.name == "ground")
+        {
+            //音を出す 
+            GetComponent<AudioSource>().Play();
+
+        }
+        //衝突したオブジェクトが箱の場合
+        else if (other.gameObject.name == "CubePrefab(Clone)")
+         {
+            //音を出す
+            GetComponent<AudioSource>().Play();
+        }
+
+    }
+
+}
